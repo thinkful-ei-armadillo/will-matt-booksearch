b7f3c8899de8c7bbc8207cb4cd0ce7e9f657e09e
William Wong <wwong1987@gmail.com>
2019-02-11 08:11:16 -0800

    added component folders and files

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import React from 'react';
 function App() {
   return (
     <main className='App'>
+      <h1>Google Book Search</h1>
       {/* content goes here */}
     </main>
   );
 }
+
+// search bar
+// book type filter
+// print type filter
+// book search list results
+
+
+
+
+
+
+
 
 export default App;
